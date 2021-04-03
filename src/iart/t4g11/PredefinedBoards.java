7e39ff0fbeffd3246bdadbd33c871b5650401fee
Zé Pedro <nepsname@gmail.com>
2021-04-03 20:50:32 +0000

    Images and performance chart

--- src/iart/t4g11/PredefinedBoards.java
+++ src/iart/t4g11/PredefinedBoards.java
@@ -17,7 +17,7 @@ public class PredefinedBoards {
         boards6x6.add(board6_9);
         boards6x6.add(board6_10);
 
-        boards10x10.add(board10_1);
+        boards10x10.add(board10_5);
         boards10x10.add(board10_2);
         boards10x10.add(board10_3);
         boards10x10.add(board10_4);
@@ -146,7 +146,7 @@ public class PredefinedBoards {
             new int[][] {new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}});
 
     private final Board board10_2 = new Board(
-            "Puzzle #4",
+            "Puzzle #2",
             Difficulty.EASY,
             PuzzleSize.TEN,
             new int[] {3, 5, 5, 9, 8, 8, 3, 9, 5, 8},
@@ -155,7 +155,7 @@ public class PredefinedBoards {
             new int[][] {new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}});
 
     private final Board board10_3 = new Board(
-            "Puzzle #6",
+            "Puzzle #3",
             Difficulty.EASY,
             PuzzleSize.TEN,
             new int[] {4, 3, 3, 3, 5, 1, 7, 3, 1, 2},
@@ -164,7 +164,7 @@ public class PredefinedBoards {
             new int[][] {new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}});
 
     private final Board board10_4 = new Board(
-            "Puzzle #8",
+            "Puzzle #4",
             Difficulty.EASY,
             PuzzleSize.TEN,
             new int[] {5, 6, 7, 8, 8, 3, 6, 5, 8, 9},
@@ -173,7 +173,7 @@ public class PredefinedBoards {
             new int[][] {new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}});
 
     private final Board board10_5 = new Board(
-            "Puzzle #9",
+            "Puzzle #5",
             Difficulty.EASY,
             PuzzleSize.TEN,
             new int[] {3, 1, 1, 1, 1, 3, 3, 2, 7, 8},
@@ -182,7 +182,7 @@ public class PredefinedBoards {
             new int[][] {new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}});
 
     private final Board board10_6 = new Board(
-            "Puzzle #10",
+            "Puzzle #6",
             Difficulty.EASY,
             PuzzleSize.TEN,
             new int[] {5, 7, 6, 7, 9, 7, 4, 2, 6, 6},
